a4f1493c768f7c724cff7335d3f9261e016b7693
Алексей <alexey712003@list.ru>
2019-09-26 19:54:27 +0300

    Правка 3

--- search.php
+++ search.php
@@ -2,7 +2,7 @@
 require_once ('helpers.php');
 
 $link = mysqli_connect('localhost', 'root', '', 'YetiCave');
-if ($link == false) {
+if (!$link) {
     print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
     die();
 }
